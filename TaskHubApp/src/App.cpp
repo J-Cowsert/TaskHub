792eb7ad64c7aa27c4b1b7f795f643f7ba71c6ea
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-05-26 15:05:29 -0500

    Test new features

--- TaskHubApp/src/App.cpp
+++ TaskHubApp/src/App.cpp
@@ -2,12 +2,22 @@
 #include "Core/Application.h"
 #include "Core/EntryPoint.h"
 
+class ToolDemo : public taskhub::Layer {
+public:
+
+	void OnUIRender() override {
+		ImGui::ShowDemoWindow();
+		ImGui::ShowStyleEditor();
+	}
+};
 
 taskhub::Application* taskhub::CreateApplication() {
 
 	taskhub::ApplicationProvision provision;
 	provision.Name = "Test";
+
 	taskhub::Application* app = new taskhub::Application(provision);
+	app->PushLayer<ToolDemo>();
 
 	return app;
 }
