f843223fa2a65300ab331382a93b883bc486c2d3
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-05-30 15:58:42 -0500

    Add public access specifier to methods in Input.h

--- TaskHub/src/Input/Input.h
+++ TaskHub/src/Input/Input.h
@@ -8,7 +8,7 @@ struct ImVec2;
 namespace taskhub {
 
 	class Input {
-
+	public:
 		static bool IsKeyPressed(KeyCode key);
 		static bool IsMouseButtonPressed(MouseCode mousecode);
 		static ImVec2 GetMousePos();
