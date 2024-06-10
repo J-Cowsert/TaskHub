92ad918b7d0780bc3ed82e6b33428c75a09186a7
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-06-10 10:01:54 -0500

    Fix main thread performance issues

--- TaskHub/src/Core/Application.cpp
+++ TaskHub/src/Core/Application.cpp
@@ -53,12 +53,12 @@ namespace taskhub {
 
 			{
 				static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
-
+			
 				// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
 				// because it would be confusing to have two docking targets within each others.
 				ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDocking;
 				
-
+			
 				const ImGuiViewport* viewport = ImGui::GetMainViewport();
 				ImGui::SetNextWindowPos(viewport->WorkPos);
 				ImGui::SetNextWindowSize(viewport->WorkSize);
@@ -67,12 +67,12 @@ namespace taskhub {
 				ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
 				window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
 				window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-
+			
 				// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background
 				// and handle the pass-thru hole, so we ask Begin() to not render a background.
 				if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
 					window_flags |= ImGuiWindowFlags_NoBackground;
-
+			
 				// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
 				// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive,
 				// all active windows docked into it will lose their parent and become undocked.
@@ -81,22 +81,24 @@ namespace taskhub {
 				ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 				ImGui::Begin("DockSpace Demo", nullptr, window_flags);
 				ImGui::PopStyleVar();
-
+			
 				ImGui::PopStyleVar(2);
-
+			
 				// Submit the DockSpace
 				if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
 				{
 					ImGuiID dockspace_id = ImGui::GetID("AppDockspace");
 					ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
 				}
-
+			
 				for (auto& layer : m_LayerStack) {
 					layer->OnUIRender();
 				}
 
 				ImGui::End();
 			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(5));
 
 			// Rendering
 			glfwSwapBuffers(m_Window);
