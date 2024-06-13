64aeef7c973f131df958bb3059966c0424cbb769
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-06-13 15:01:45 -0500

    Add UIUtils to assist building applications

---
+++ TaskHub/src/Gui/UI/UIUtils.cpp
@@ -0,0 +1,34 @@
+#include "UIUtils.h"
+
+namespace taskhub::UI {
+
+	void ShiftCursor(float x, float y) {
+
+		const ImVec2 cursor = ImGui::GetCursorPos();
+		ImGui::SetCursorPos(ImVec2(cursor.x + x, cursor.y + y));
+	}
+
+	void ShiftCursorX(float distance) {
+		ImGui::SetCursorPosX(ImGui::GetCursorPosX() + distance);
+	}
+
+	void ShiftCursorY(float distance) {
+		ImGui::SetCursorPosY(ImGui::GetCursorPosY() + distance);
+	}
+
+	void AlignCursorForWidth(float width, float alignment = 0.5f) {
+
+		float availWidth = ImGui::GetContentRegionAvail().x;
+		float offsetX = (availWidth - width) * alignment;
+		if (offsetX > 0.0f)
+			ImGui::SetCursorPosX(ImGui::GetCursorPosX() + offsetX);
+	}
+
+	void AlignCursorForHeight(float height, float alignment = 0.5f) {
+
+		float availHeight = ImGui::GetContentRegionAvail().y;
+		float offsetY = (availHeight - height) * alignment;
+		if (offsetY > 0.0f)
+			ImGui::SetCursorPosX(ImGui::GetCursorPosY() + offsetY);
+	}
+}
---
+++ TaskHub/src/Gui/UI/UIUtils.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "imgui.h"
+#include "imgui_internal.h"
+#include <memory>
+
+namespace taskhub::UI {
+
+	void ShiftCursor(float x, float y);
+	void ShiftCursorX(float distance);
+	void ShiftCursorY(float distance);
+	void AlignCursorForWidth(float width, float alignment);
+	void AlignCursorForHeight(float height, float alignment);
+}
