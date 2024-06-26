4400b14d35ec8ef70e91f51abd9d9f88b37a92d8
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-06-26 15:34:40 -0500

    Add Image feature to TaskHub

---
+++ TaskHub/src/Core/Image.cpp
@@ -0,0 +1,124 @@
+#include "Image.h"
+#include "Assert.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+namespace taskhub {
+
+	namespace Utils {
+
+		static uint32_t BytesPerPixel(ImageFormat format) {
+
+			switch (format) 
+			{
+				case ImageFormat::RGBA:      return 4;
+				case ImageFormat::RGBA32F:   return 16;
+			}
+
+			return 0;
+		}
+
+		static GLint TaskHubFormatToOpenGLFormat(ImageFormat format) {
+
+			switch (format) 
+			{
+				case ImageFormat::RGBA:      return GL_RGBA8;
+				case ImageFormat::RGBA32F:   return GL_RGBA32F;
+			}
+
+			return 0;
+		}
+	}
+
+	Image::Image(std::string_view path)
+		: m_FilePath(path)
+	{
+		HUB_CORE_ASSERT(std::filesystem::exists(m_FilePath), "Filepath does not exist");
+
+		int width, height, channels;
+		uint8_t* data = nullptr;
+
+		if (stbi_is_hdr(m_FilePath.c_str())) {
+			
+			data = (uint8_t*)stbi_loadf(m_FilePath.c_str(), &width, &height, &channels, 4);
+			m_ImageFormat = ImageFormat::RGBA32F;
+			HUB_CORE_INFO("HDR Image loaded");
+		}
+		else {
+			data = stbi_load(m_FilePath.c_str(), &width, &height, &channels, 4);
+			m_ImageFormat = ImageFormat::RGBA;
+			HUB_CORE_INFO("Image loaded");
+		}
+
+		if (data == nullptr)
+			HUB_CORE_CRITICAL("Image data is null");
+
+		m_Width = width;
+		m_Height = height;
+
+		SetData(data);
+		stbi_image_free(data);
+	}
+
+	Image::Image(uint32_t width, uint32_t height, ImageFormat format, const void* data)
+		: m_Width(width), m_Height(height), m_ImageFormat(format)
+	{
+		if (data)
+			SetData(data);
+	}
+
+	Image::~Image() {
+		ReleaseMemory();
+	}
+
+	void Image::SetData(const void* data) {
+
+		glGenTextures(1, &m_TextureID);
+		glBindTexture(GL_TEXTURE_2D, m_TextureID);
+
+		GLint internalFormat = Utils::TaskHubFormatToOpenGLFormat(m_ImageFormat);
+		GLenum type = (m_ImageFormat == ImageFormat::RGBA32F) ? GL_FLOAT : GL_UNSIGNED_BYTE;
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+		glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, m_Width, m_Height, 0, GL_RGBA, type, data);
+
+		glBindTexture(GL_TEXTURE_2D, 0); // unbind the texture
+	}
+
+	void Image::Resize(uint32_t width, uint32_t height) {
+
+		if (m_TextureID && m_Width == width && m_Height == height)
+			return;
+
+		m_Width = width;
+		m_Height = height;
+	}
+
+	void* Image::DecodeFromMemory(const void* buffer, uint64_t length, uint32_t& outWidth, uint32_t& outHeight) {
+
+		int width, height, channels;
+		uint8_t* data = nullptr;
+		uint64_t size = 0;
+
+		data = stbi_load_from_memory((const stbi_uc*)buffer, length, &width, &height, &channels, 4);
+		size = width * height * 4;
+
+		outWidth = width;
+		outHeight = height;
+
+		return data;
+	}
+
+	void Image::ReleaseMemory() {
+
+		if (m_TextureID) {
+			glDeleteTextures(1, &m_TextureID);
+			m_TextureID = 0;
+		}
+	}
+}
---
+++ TaskHub/src/Core/Image.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <glad/glad.h>
+#include <string>
+#include <filesystem>
+
+namespace taskhub {
+
+	enum class ImageFormat {
+		None = 0, 
+		RGBA,
+		RGBA32F
+	};
+
+	class Image {
+	public:
+		Image(std::string_view path);
+		Image(uint32_t width, uint32_t height, ImageFormat format, const void* data = nullptr);
+		~Image();
+
+		void SetData(const void* data);
+		void Resize(uint32_t width, uint32_t height);
+
+		GLuint GetTextureID() const { return m_TextureID; }
+		uint32_t GetWidth() const { return m_Width; }
+		uint32_t GetHeight() const { return m_Height; }
+
+		static void* DecodeFromMemory(const void* buffer, uint64_t length, uint32_t& outWidth, uint32_t& outHeight);
+		
+	private:
+		void ReleaseMemory();
+
+	private:
+
+		uint32_t m_Width = 0, m_Height = 0;
+		GLuint m_TextureID = 0;
+		std::string m_FilePath;
+		ImageFormat m_ImageFormat = ImageFormat::None;
+	};
+}
