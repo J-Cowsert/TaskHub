73f72ea743393a8eec6383ac42187d356bd5fbfe
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-07-06 12:15:32 -0500

    Adjust background color

--- TaskHub/src/GUI/ImGuiStyle.cpp
+++ TaskHub/src/GUI/ImGuiStyle.cpp
@@ -8,7 +8,7 @@ namespace taskhub::UI {
 
          colors[ImGuiCol_Text]                  = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
          colors[ImGuiCol_TextDisabled]         	= ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-         colors[ImGuiCol_WindowBg]              = ImVec4(0.15f, 0.15f, 0.15f, 1.00f);
+         colors[ImGuiCol_WindowBg]              = ImVec4(0.25f, 0.25f, 0.25f, 1.00f);
          colors[ImGuiCol_ChildBg]               = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
          colors[ImGuiCol_PopupBg]               = ImVec4(0.19f, 0.19f, 0.19f, 0.92f);
          colors[ImGuiCol_Border]                = ImVec4(0.19f, 0.19f, 0.19f, 0.29f);
