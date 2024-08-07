4ee86b979f7d3e79160175136061452140c41bb2
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-08-06 19:58:10 -0500

    Refactor Timer class

--- TaskHub/src/Time/Timer.cpp
+++ TaskHub/src/Time/Timer.cpp
@@ -2,13 +2,10 @@
 
 namespace taskhub {
 
-	Timer::Timer()
-		: m_Running(false), m_Duration(0.0f), m_Elapsed(0.0f)
+	Timer::Timer(std::function<void()> timerCompletedCallback)
+		: m_Running(false), m_Completed(false), m_Duration(std::chrono::duration<float>(0.0f)),
+		  m_Elapsed(std::chrono::duration<float>(0.0f)), m_TimerCompletedCallback(timerCompletedCallback)
 	{
-	}
-
-	void Timer::SetTimer(float duration) {
-		m_Duration = std::chrono::duration<float>(duration);
 	}
 
 	void Timer::Start() {
@@ -30,9 +27,12 @@ namespace taskhub {
 
 	void Timer::Reset() {
 		m_Running = false;
+		m_Completed = false;
 		m_Elapsed = std::chrono::duration<float>(0.0f);
 	}
 
+	// Must be called consecutivly for timer to function properly. 
+	// TODO: Redesign class to be multithreaded to function on its own
 	float Timer::GetRemainingTime() {
 
 		if (!m_Running) {
@@ -40,13 +40,20 @@ namespace taskhub {
 		}
 		else {
 			auto now = std::chrono::high_resolution_clock::now();
-			float duration = std::chrono::duration<float>(m_Duration - m_Elapsed - (now - m_Start)).count();
-			float remTime = (duration >= 0) ? duration : 0.0f;
-			return remTime;
+			float remainingTime = std::chrono::duration<float>(m_Duration - m_Elapsed - (now - m_Start)).count();
+
+			// 
+			if (remainingTime <= 0) {
+				m_Completed = true;
+				if (m_TimerCompletedCallback)
+					m_TimerCompletedCallback();
+			}
+
+			return (remainingTime >= 0) ? remainingTime : 0.0f;
 		}
 	}
 
-	float Timer::GetElapsedTime() {
+	float Timer::GetElapsedTime() const {
 
 		if (!m_Running) {
 			return m_Elapsed.count();
--- TaskHub/src/Time/Timer.h
+++ TaskHub/src/Time/Timer.h
@@ -1,27 +1,41 @@
 #pragma once
 
 #include <chrono>
+#include <functional>
 
 namespace taskhub {
 
 	class Timer {
 	public:
-		Timer();
+		explicit Timer(std::function<void()> timerCompletedCallback);
 		~Timer() = default;
 
-		void SetTimer(float duration); // Only In seconds for now 
+		template<typename DurationType>
+		void SetTimer(float duration) {
+			auto durationInInt = static_cast<typename DurationType::rep>(duration);
+			m_Duration = DurationType(durationInInt);
+		}
 
+		void SetCompletedCallback(std::function<void()>& callback) { m_TimerCompletedCallback = callback; }
+		
 		void Start();
 		void Stop();
 		void Reset();
 
 		float GetRemainingTime();
-		float GetElapsedTime();
+		float GetElapsedTime() const;
+
+		bool IsCompleted() const { return m_Completed; }
+		bool IsRunning() const { return m_Running; }
 
 	private:
 		bool m_Running;
+		bool m_Completed;
+
 		std::chrono::duration<float> m_Duration;
 		std::chrono::duration<float> m_Elapsed;
 		std::chrono::time_point<std::chrono::high_resolution_clock> m_Start;
+
+		std::function<void()> m_TimerCompletedCallback;
 	};
 }
