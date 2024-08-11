8c196adc3338be842214fa6ba159bd84f0083fdd
James Cowsert <148249109+DexNipp@users.noreply.github.com>
2024-08-11 15:43:18 -0500

    Remove log output from entrypoint

--- TaskHub/src/Core/EntryPoint.h
+++ TaskHub/src/Core/EntryPoint.h
@@ -4,8 +4,6 @@
 int main(int argc, char** argv) {
 
 	taskhub::Log::Init();
-	HUB_CORE_INFO("Log::Init in entrypoint\n");
-
 	taskhub::Application* App = taskhub::CreateApplication();
 	App->Run();
 	delete App;
